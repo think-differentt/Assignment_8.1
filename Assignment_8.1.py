deb9174893ec0a1ddc1ccfc1d17e0b29668bf756
think-differentt <71799028+think-differentt@users.noreply.github.com>
2020-10-04 19:42:45 -0500

    Add files via upload

---
+++ Assignment_8.1.py
@@ -0,0 +1,31 @@
+#<Justin Carter>
+#<04OCT2020>
+#Writing a Python program that has you interacting with the file system.
+
+import os
+
+#Prompt the user for the name of the directory
+fileLocation = input("Please enter the file path:")
+
+#Prompt the user for the name of the file
+fileTitle = input("Please enter the file name:")
+
+#if the directory doesn't exist the program must create the specified directory
+if not os.path.exists(fileLocation):
+    os.makedirs(fileLocation)
+entireEntry = os.path.join(fileLocation, fileTitle + ".txt")
+
+
+#Prompt the user for their name, address, and phone number 
+fileData = open(entireEntry, "w")
+userName = input("Please enter a name:")
+userAddress = input("Please enter an address:")
+userPhone = input("Please enter a phone number:")
+
+#write that data as a line of comma separated values to the file
+fileData.write(userName+', '+userAddress+', '+userPhone+'\n')
+fileData.close()
+
+#After the data has been written to the file your program must open the file, read the contents, and display the contents to the user as program output.
+fileData = open(entireEntry, "r")
+print(fileData.read())
